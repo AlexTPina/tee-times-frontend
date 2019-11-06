963c64ef416ad81120243cacc3621ac9310553aa
Alex Pina <alexpina16@msn.com>
2019-11-05 20:18:42 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux'
+import { fetchAllCustomers } from './store/customers/actions'
+import { fetchAllTeetimes } from './store/teetimes/actions'
 
+const styles = {
+  noPadding: {
+    padding: 0,
+  },
+}
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchAllCustomers)
+    this.props.dispatch(fetchAllTeetimes)
+  }
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
